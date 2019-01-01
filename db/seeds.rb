7aeef4af859dcb7168e1aefe956e6294acbff8ea
EC2 Default User <ec2-user@ip-172-31-41-114.ap-southeast-1.compute.internal>
2019-01-01 06:56:41 +0000

    add info seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Information.create(lastname: "aa", firstname: "aa", lastnamekana: "スズキ", firstnamekana: "タロウ", email: "aaa@gmail.com", address: "aa", phonenumber1: "0000", phonenumber2: "1111", phonenumber3: "1111")
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -14,4 +16,3 @@
 #   Information.create(lastname: row[1], firstname: row[2], lastnamekana: row[3], firstnamekana: row[4], email: row[5], address: row[6], phonenumber1: row[7], phonenumber2: row[8], phonenumber3: row[9])
 # end
 
-Information.create(lastname: "aa", firstname: "aa", lastnamekana: "スズキ", firstnamekana: "タロウ", email: "aaa@gmail.com", address: "aa", phonenumber1: 0000, phonenumber2: 1111, phonenumber3: 1111)
