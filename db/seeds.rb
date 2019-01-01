749e10aed986b699f010c175946054789d3aeadc
EC2 Default User <ec2-user@ip-172-31-41-114.ap-southeast-1.compute.internal>
2019-01-01 07:49:08 +0000

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Information.create(lastname: "aa", firstname: "aa", lastnamekana: "スズキ", f
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+#hit Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
