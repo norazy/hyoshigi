e51fbc0662688518edcc2ad4124e4dbef5e7da9d
EC2 Default User <ec2-user@ip-172-31-41-114.ap-southeast-1.compute.internal>
2019-01-01 04:44:17 +0000

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Inquiry.create(lastname: '拍子木', firstname: '太郎', lastnamekana: 'ヒョウシギ', firstnamekana: 'タロウ', email: 'example@gmail.com', content: 'もしもし')
+Inquiry.create(lastname: '拍子木', firstname: '太郎')
